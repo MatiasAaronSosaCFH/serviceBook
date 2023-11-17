a4728d8792d32af84292b6b1e5d07571d14a623c
Matias <akucfh@gmail.com>
2023-11-17 19:16:00 -0300

    primerCommit

---
+++ src/main/java/com/servicebook/ServiceBookApplication.java
@@ -0,0 +1,13 @@
+package com.servicebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBookApplication.class, args);
+	}
+
+}
