6675a7b3f036365b66a7886629c078bf48d1aa2e
Julieta Pilili <julietapilili101@gmail.com>
2023-11-22 19:21:50 -0300

    salio mal

--- src/main/java/com/servicebook/models/Profesion.java
+++ src/main/java/com/servicebook/models/Profesion.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 
 @Entity
--- src/main/java/com/servicebook/repository/TrabajoRepository.java
+++ src/main/java/com/servicebook/repository/TrabajoRepository.java
@@ -1,9 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.servicebook.repository;
 
+import com.servicebook.models.Trabajo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
