ac39ada3b49581a916bb681bfcba78ee9d659b98
Julieta Pilili <julietapilili101@gmail.com>
2023-11-23 18:31:43 -0300

    entidad trabajo y controller

--- src/main/java/com/servicebook/controller/TrabajoController.java
+++ src/main/java/com/servicebook/controller/TrabajoController.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.servicebook.controller;
 
-import com.servicebook.service.ProfesionService;
+import com.servicebook.models.Trabajo;
+import com.servicebook.service.TrabajoService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,10 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/trabajo")
 public class TrabajoController {
-    
+
     @Autowired
-    private ProfesionService profesionService;
-    
-    
-    
+    private TrabajoService trabajoService;
+
+    @GetMapping("/lista")
+    public String listar(ModelMap model) {
+        List<Trabajo> trabajos = trabajoService;
+        model.addAttribute("trabajos", trabajos);
+        return "/lista";
+    }
+
 }
--- src/main/java/com/servicebook/service/TrabajoService.java
+++ src/main/java/com/servicebook/service/TrabajoService.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.servicebook.service;
+
+import com.servicebook.repository.TrabajoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author julip
  */
+
+@Service
 public class TrabajoService {
+    
+    @Autowired
+    private TrabajoRepository trabajoRepository;
+    
     
 }
